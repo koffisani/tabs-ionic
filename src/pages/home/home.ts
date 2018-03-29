ceda1031c3d474c2e8d7c1a6bb13178887d4c7d3
Koffi Sani <koffisani@gmail.com>
2018-03-29 07:56:37 +0000

    Ajout d'une fenêtre modale de paramétrage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 
 import 'rxjs/add/operator/map';
 import { ProductProvider } from '../../providers/product/product';
 import { ProductDetailPage } from '../product-detail/product-detail';
+import { FilterModalPage } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'page-home',
@@ -15,7 +16,8 @@ export class HomePage {
 
   public products = [];
 
-  constructor(private product: ProductProvider, 
+  constructor(private product: ProductProvider,
+    private modalController: ModalController, 
     private http: Http,
     public navCtrl: NavController) {
 
@@ -33,6 +35,11 @@ export class HomePage {
     this.navCtrl.push(ProductDetailPage, {
       productDetails: produit
     })
+  }
+
+  openFilterModal () {
+    let openFilterModal = this.modalController.create(FilterModalPage);
+    openFilterModal.present();
   }
 
 }
